4890faaa1061c0037c82fac201d7baea35fba3d1
Ptunc <spolentunc@gmail.com>
2022-10-18 16:43:00 +0300

    just commit

---
+++ java8.java
@@ -0,0 +1,29 @@
+package Java8;
+
+import java.util.Scanner;
+
+public class java8 {
+    public static void main(String[] args) {
+        
+        int a,b;
+        int islem;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("a: ");
+        a = input.nextInt();
+        System.out.print("b: ");
+        b = input.nextInt();
+        
+        System.out.print("sayiyi girin;\n 1. /\n 2.*\n 3.+\n 4.-\n");
+        islem = input.nextInt();
+
+        switch(islem){
+            case 1: System.out.println(a/b); break;
+            case 2: System.out.println(a*b); break;
+            case 3: System.out.println(a+b); break;
+            case 4: System.out.println(a-b); break;
+            default: System.out.println("yanlış karakter girdiniz.");
+        }
+        
+    }
+}
